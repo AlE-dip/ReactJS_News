614e7b2976601a5ba4b95f59dd5961d25b570190
AlE-dip <71812422+AlE-dip@users.noreply.github.com>
2022-03-17 09:58:26 +0700

    Revert "news"
    This reverts commit 6d340ae894f1b9e4af5584f0a2916680db541122.

--- news/src/App.js
+++ news/src/App.js
@@ -3,64 +3,60 @@ import './App.css';
 import Home from './pages/Home';
 import Header from './pages/Header'
 import Footer from './pages/Footer'
-import News from './pages/News'
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter } from "react-router-dom";
 import About from './pages/About';
 
 
 class App extends Component {
 
-    state = {
-        data: 'APP'
-    };
+  state = {
+    data: 'APP'
+  };
 
-    componentDidMount() {
-        this.callBackendAPI()
-            // .then(res => this.setState({ data: res.rss }))
-            .then(res => {
-                var arr = res.rss.channel[0].item
-                console.log('rss', arr)
-                this.setState({ data: arr })
-            })
-            .catch(err => console.log(err));
+  componentDidMount() {
+    this.callBackendAPI()
+      // .then(res => this.setState({ data: res.rss }))
+      .then(res => {
+        var arr = res.rss.channel[0].item
+        console.log('rss', arr)
+        this.setState({ data: arr })
+      })
+      .catch(err => console.log(err));
+  }
+  // fetching the GET route from the Express server which matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+    console.log('RSS', body)
+
+    if (response.status !== 200) {
+      throw Error(body.message)
     }
-    // fetching the GET route from the Express server which matches the GET route from server.js
-    callBackendAPI = async () => {
-        const response = await fetch('/express_backend');
-        const body = await response.json();
-        console.log('RSS', body)
+    return body;
+  };
 
-        if (response.status !== 200) {
-            throw Error(body.message)
-        }
-        return body;
-    };
+  home = () => {
+    return(
+      <Home dataFromParent={this.state.data}/>
+    )
+  }
 
-    home = () => {
-        return (
-            <Home dataFromParent={this.state.data} />
-        )
-    }
+  render() {
+    return (
 
-    render() {
-        return (
-
-            <div>
-                
-                    <Header />
-
-                    <Route exact path="/" component={<Home dataFromParent={this.state.data} />} />
-                    <Route path="/about" component={About} />
-                    <Route path="/news" component={News} />
-
-                    <Footer />
-                {/* <div dangerouslySetInnerHTML={{ __html: this.state.data }} /> */}
-            </div>
+      <div>
+        <Header />
+        <BrowserRouter>
+          <Route exact path="/" component={this.home} />
+          <Route path="/about" component={About} />
+        </BrowserRouter>
+        <Footer />
+        {/* <div dangerouslySetInnerHTML={{ __html: this.state.data }} /> */}
+      </div>
 
 
-        );
-    }
+    );
+  }
 }
-
 
 export default App;
