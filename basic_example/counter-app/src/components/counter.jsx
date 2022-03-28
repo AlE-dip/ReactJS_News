72041014ecf867e298d8f281f7bfe29976359e18
aquoc13 <aquoc13.s@gmail.com>
2022-03-28 13:48:09 +0700

    Adding basix example

---
+++ basic_example/counter-app/src/components/counter.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+//First example
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    this.state.count++;
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "btn m-2 btn-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
